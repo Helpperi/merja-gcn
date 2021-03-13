5f72ca8f032726f666a41d48b01f00d70cb31caa
Lasse Häll <LasseHall@users.noreply.github.com>
2021-03-13 11:31:00 +0200

    reduce title font size

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,11 +2,10 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const Title = styled.h1`
-  font-size: ${props => (props.small ? '2em' : '3em')};
+  font-size: ${props => (props.small ? '1em' : '2em')};
   font-family: ${props => props.theme.fonts.header};
   font-weight: 600;
   text-align: center;
-  margin: 0 0 3rem 0;
   margin: ${props => (props.small ? '1rem 0 4rem 0' : '0 0 3rem 0')};
   line-height: 1.2;
   span {
