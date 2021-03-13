3f6a91347e32af8a66430d7f4c7aab068a2cdabe
Lasse Häll <LasseHall@users.noreply.github.com>
2021-03-13 23:31:57 +0200

    Update PostLinks.js

--- src/components/PostLinks.js
+++ src/components/PostLinks.js
@@ -42,12 +42,12 @@ const PostLinks = props => {
       <Box>
         {props.previous && (
           <PreviousLink to={`${props.basePath}/${props.previous.slug}/`}>
-            &#8592; Prev
+            &#8592; Edellinen
           </PreviousLink>
         )}
         {props.next && (
           <NextLink to={`${props.basePath}/${props.next.slug}/`}>
-            Next &#8594;
+            Seuraava &#8594;
           </NextLink>
         )}
       </Box>
